df55612cb90c302f911029df1d96636ab91d5a0f
alycen <alsunnid@gmail.com>
2015-12-14 15:00:57 +0000

    Attempting to disable button

---
+++ Assets/Scripts/ButtonDisable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonDisable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DisableButton()
+    {
+        this.transform.position = new Vector3(-10000,50000,0);
+    }
+}
